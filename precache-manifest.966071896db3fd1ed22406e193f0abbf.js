5a0d31ddc3f6cbae196016b9e1aa0c9caf09fc03
Tim <tim@tuvainteractive.com>
2019-11-15 12:19:03 -0600

    Updates

---
+++ precache-manifest.966071896db3fd1ed22406e193f0abbf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "66111076b9529b3eeea9a64fe97b1abe",
+    "url": "/titotortellini/cautious-octo-doodle/index.html"
+  },
+  {
+    "revision": "94c03c2163d447a51c2d",
+    "url": "/titotortellini/cautious-octo-doodle/static/css/main.92a7f898.chunk.css"
+  },
+  {
+    "revision": "d00b06e5b9719b72550c",
+    "url": "/titotortellini/cautious-octo-doodle/static/js/2.f6b3e6dd.chunk.js"
+  },
+  {
+    "revision": "94c03c2163d447a51c2d",
+    "url": "/titotortellini/cautious-octo-doodle/static/js/main.d30ab58a.chunk.js"
+  },
+  {
+    "revision": "ed64fc619ed3ca3a02bb",
+    "url": "/titotortellini/cautious-octo-doodle/static/js/runtime-main.17146e93.js"
+  },
+  {
+    "revision": "c0bd114521dac5c08ae551dc0ed03af5",
+    "url": "/titotortellini/cautious-octo-doodle/static/media/tw2.c0bd1145.svg"
+  }
+]);
